0281e78493493a3834f2d6cdfdea190e17747be2
Alex <Rieznik.O.A@nmu.one>
2026-02-25 20:53:21 +0200

    Solution#2

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from django.db.models import QuerySet
 
 from db.models import Genre, Actor
 
+genres = ["Western", "Action", "Dramma"]
+actors = [("George", "Klooney"),
+          ("Kianu", "Reaves"),
+          ("Scarlett", "Keegan"),
+          ("Will", "Smith"),
+          ("Jaden", "Smith"),
+          ("Scarlett", "Johansson")
+          ]
+
 
 def main() -> QuerySet:
     # Create
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    for genre in genres:
+        Genre.objects.create(name=genre)
+    for firstname, lastname in actors:
+        Actor.objects.create(first_name=firstname, last_name=lastname)
 
     # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
